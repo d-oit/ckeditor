57364a273c7253858f5c98c13431e52beb893742
dosoft <dmmotec@gmail.com>
2016-04-28 20:37:18 +0200

    Changes

--- ckeditor_4.5.8_44c6ba148f25/ckeditor/plugins/insertbr/plugin.js
+++ ckeditor_4.5.8_44c6ba148f25/ckeditor/plugins/insertbr/plugin.js
@@ -1,27 +1,19 @@
 CKEDITOR.plugins.add('insertbr',
 {
     init: function (editor) {
-
         var pluginName = 'insertbr';
-
         editor.ui.addButton('insertbr',
-
             {
                 label: 'Insert BR',
                 command: 'NewLine',
                 icon: CKEDITOR.plugins.getPath('insertbr') + 'icons/InsertParagraph.png'
             });
 
-  var cmd = editor.addCommand('NewLine', { exec: showNewLine });
-
+	var cmd = editor.addCommand('NewLine', { exec: insertbr });
     }
 
 });
 
-function showNewLine(e) {
-
+function insertbr(e) {
    e.execCommand( 'shiftEnter' );
-
-   // Here if I replace the above line with  e.insertHtml('<br />&nbsp;'); it will work fine but is adding an extra space at the beggining of each line.
-
 }
